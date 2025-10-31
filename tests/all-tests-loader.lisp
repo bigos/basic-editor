2e19c04096c209508b81de996dc81ab9bf9be8b3
Jacek Podkanski <ruby.object@googlemail.com>
2025-10-31 22:53:32 +0000

    all tests loader

---
+++ tests/all-tests-loader.lisp
@@ -0,0 +1,12 @@
+;;; all rests loader
+
+;;; run with:
+;; sbcl --load ./all-tests-loader.lisp
+
+(ql:quickload :basic-editor/tests)
+
+(in-package #:basic-editor-test)
+
+(test-all)
+
+(sb-ext:quit)
