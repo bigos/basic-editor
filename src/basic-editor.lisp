b596940880f25eb4ce1e685ebb4b7093ad40606f
Jacek Podkanski <ruby.object@googlemail.com>
2026-03-07 20:27:38 +0000

    cleanup of the statistics

--- src/basic-editor.lisp
+++ src/basic-editor.lisp
@@ -115,44 +115,35 @@
    (col cursor) last-col))
 ;;; ============================================================================
 (defmethod cursor-stats ((model basic-editor-model))
-  (let ((length-text (length (text model))))
-    (list :finish-me
-          (list
-           "text length" length-text
-           (text-stats (text model))))))
+  (text-stats (text model)))
 
 (defun text-stats (text)
-  (let ((length-text (length text)))
-    (list :finish-me
-          (list
-           "text length" length-text)
-          (loop
-            for oldri = 0 then ri
-            for c across (format nil text )
-            for i = 0 then (1+ i)
-            for col = 0 then (if (eq c #\Newline) 0 (1+ col))
-            for row = 0 then (if (eq c #\Newline) (1+ row) row)
-            for ri = 0  then (if (eq c #\Newline) (1+ i) ri)
-            collect
-            (list c "pos" i "row-col" row col "ri" ri "line"
-                  (if (eq c #\Newline)
-                      (subseq text oldri (+ 1 i))
-                      (subseq text ri (1+ i))))
-              into collected-characters
-            when (eq  c #\Newline)
-              collect (list c
-                            "pos" i
-                            "row-col" row col
-                            (subseq text (- i col) (1+ i)))
-                into collected-newlines
-            finally (return (list
-                             "collected newlines" collected-newlines
-                             "last character"(list c
-                                                   "pos" i
-                                                   "row-col" row col
-                                                   )
-                             "collected characters" collected-characters
-                             ))))))
+  (loop
+    for oldri = 0 then ri
+    for c across (format nil text )
+    for i = 0 then (1+ i)
+    for col = 0 then (if (eq c #\Newline) 0 (1+ col))
+    for row = 0 then (if (eq c #\Newline) (1+ row) row)
+    for ri = 0  then (if (eq c #\Newline) (1+ i) ri)
+    collect
+    (list :char c :pos i :row-col (cons row col) :ri ri :line
+          (if (eq c #\Newline)
+              (subseq text oldri (+ 1 i))
+              (subseq text ri (1+ i))))
+      into collected-characters
+    when (eq  c #\Newline)
+      collect (list :char c
+                    :pos i
+                    :row-col (cons row col)
+                    (subseq text (- i col) (1+ i)))
+        into collected-newlines
+    finally (return (list
+                     :text-length (or i 0)
+                     :collected-newlines collected-newlines
+                     :last-character (list :char c
+                                           :pos i
+                                           :row-col (cons row col))
+                     :collected-characters collected-characters))))
 
 (defun is-first-line (model)
   (zerop  (~> model cursor row)))
