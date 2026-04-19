d50becd445425ccae5767e47be6cb3f9a116fd66
Jacek Podkanski <ruby.object@googlemail.com>
2026-04-19 23:16:27 +0100

    removed old code

--- src/basic-editor.lisp
+++ src/basic-editor.lisp
@@ -489,32 +489,6 @@
         :if-exists :supersede
         :if-does-not-exist :create)))))
 
-;; -(defun sample-text-stats (text)
-;; -  (assert (typep text 'simple-array))
-;; -  (let ((lines-hash-table (make-hash-table)))
-;; -    (labels
-;; -        ((set-new-line (row home i)
-;; -           (warn "adding row ~S ~S ~S" row home i)
-;; -           (setf (gethash row lines-hash-table)
-;; -                 (make-instance 'text-row
-;; -                                :row row
-;; -                                :home home
-;; -                                :end i))))
-;; -      (loop
-;; -        for prevc = nil then c
-;; -        for c across text
-;; -        for i = 0 then (1+ i)
-;; -        for row =  (if (and (zerop i) (eq c #\Newline)) 0 -1) then (if (eq c #\Newline) (1+ row) row)
-;; -        for home = 0 then (if (eq prevc #\Newline) i home)
-;; -        do
-;; -           (when (eq c #\Newline)
-;; -             (set-new-line row home (1+ i)))
-;; -        finally
-;; -           (when i
-;; -             (unless (eq c #\Newline)
-;; -               (set-new-line (1+ row) home (1+ i))))))
-;; -    lines-hash-table))
-
 ;;; drawing ====================================================================
 (defun calculate-chars (text-container model)
   (let* ((the-chars
