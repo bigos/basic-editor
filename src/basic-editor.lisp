8add47c3d39be5c851ecb8df6e1457445864a9fc
Jacek Podkanski <ruby.object@googlemail.com>
2026-07-30 13:04:43 +0100

    better mouse pointer

--- src/basic-editor.lisp
+++ src/basic-editor.lisp
@@ -619,13 +619,43 @@
       (when (and (eq (gui-app:current-motion app)
                      window)
                  (gui-app:mouse-coordinates app))
-        (gui-window:set-rgba "blue")
-        (cairo:rectangle
-         (car (gui-app:mouse-coordinates app))
-         (cdr (gui-app:mouse-coordinates app))
-         25
-         25)
-        (cairo:fill-path)))))
+        ;; (gui-window:set-rgba "blue")
+        ;; (cairo:rectangle
+        ;;  (car (gui-app:mouse-coordinates app))
+        ;;  (cdr (gui-app:mouse-coordinates app))
+        ;;  25
+        ;;  25)
+        ;; (cairo:fill-path)
+    (render-mouse (gui-app:mouse-coordinates app ))))))
+
+(defun render-mouse (mouse-position)
+  (when mouse-position
+    (let* (
+           (mx (car mouse-position))
+           (my (cdr mouse-position))
+           (px (+ mx 20))
+           (py (+ my 20))
+           (po 6)
+           (ao 15))
+      (when mouse-position
+        (labels ((drrr ()
+                   (cairo:line-to (+ 50 mx) (+ po 50 my)) ; end
+                   (cairo:line-to px (+ po py))
+                   (cairo:line-to (+ ao mx) (+ 50 my))
+                   (cairo:line-to mx my)  ;mp
+                   (cairo:line-to (+ 50 mx) (+ ao my))
+                   (cairo:line-to (+ po px) py)
+                   (cairo:line-to (+ po 50 mx) (+ 50 my)) ; end
+                   (cairo:line-to (+ 50 mx) (+ po 50 my)) ; end
+                   ))
+          (drrr)
+          (cairo:set-source-rgba 0.2 1.0 0.3 0.3)
+          (cairo:fill-path)
+
+          (cairo:set-line-width 1.0)
+          (drrr)
+          (cairo:set-source-rgb 0.0 0.0 0.0) ; http://davidbau.com/colors/
+          (cairo:stroke))))))
 
 ;;; key handling ===============================================================
 (defun key-handling-f1-help ()
